f3ddaa95891e6c51601335d1cbf1451b5b1e67d4
Vitalii <witalik.karwack@gmail.com>
2026-05-21 16:05:15 -0700

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/For-The-Win-Catering/',
   plugins: [react()],
+  base: '/For-The-Win-Catering/',
 });
